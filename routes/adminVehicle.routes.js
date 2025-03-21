3ec64a8c629c87fd52fb8465b312bc77ac428bc3
Gunathilaka K K N M <nimgunathilaka021@gmail.com>
2025-03-21 19:56:22 +0530

    add route to retrieve specific vehicle by ID in adminVehicle.routes.js ✅

--- routes/adminVehicle.routes.js
+++ routes/adminVehicle.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { addVehicle, getVehicles, deleteVehicle, updateVehicle } from '../controllers/adminVehicle.controller.js';
+import { addVehicle, getVehicles, deleteVehicle, updateVehicle, getVehicle } from '../controllers/adminVehicle.controller.js';
 
 const adminVehicleRouter = express.Router();
 
 adminVehicleRouter.post("/", addVehicle);
 adminVehicleRouter.get("/", getVehicles);
+adminVehicleRouter.get("/:id", getVehicle);
 adminVehicleRouter.delete("/:id", deleteVehicle);
 adminVehicleRouter.put("/:id", updateVehicle);
 
