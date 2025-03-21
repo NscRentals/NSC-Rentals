cf1859903b1d702cdfd2220235f8ffa978067be1
christine-samandi <christinefernando816@gmail.com>
2025-03-21 13:38:51 +0530

    decoration controller fully updated

--- controllers/decorationsController.js
+++ controllers/decorationsController.js
@@ -1,105 +1,105 @@
-import Decoration from "../models/decorations.js"
+    import Decoration from "../models/decorations.js"
 
-//add decorations 
-export async function addDeco (req,res){
+    //add decorations 
+    export async function addDeco (req,res){
 
-    try{
-        let newDeco = new Decoration (req.body);
-        await newDeco.save();
-        return res.status(200).json({
-            success: "Decoration saved successfully!"
-        });
-    } catch (err){
-        return res.status(404).json({
-            error:err
-        });
-    }    
-}
+        try{
+            let newDeco = new Decoration (req.body);
+            await newDeco.save();
+            return res.status(200).json({
+                success: "Decoration saved successfully!"
+            });
+        } catch (err){
+            return res.status(404).json({
+                error:err
+            });
+        }    
+    }
 
 
-/*export async function addDeco(req, res) {
-    try {
-        let newDeco = new Decoration(req.body);
-        await newDeco.save();
-        return res.status(201).json({
-            success: true,
-            message: "Decoration saved successfully!",
-            data: newDeco
-        });
-    } catch (err) {
-        return res.status(400).json({
-            success: false,
-            error: err.message
-        });
-    }    
-}*/
-
-//get decorations
-export async function getDeco (req,res){
-
-    try{
-        const deco = await Decoration.find();
-
-        if(deco.length===0){
+    /*export async function addDeco(req, res) {
+        try {
+            let newDeco = new Decoration(req.body);
+            await newDeco.save();
+            return res.status(201).json({
+                success: true,
+                message: "Decoration saved successfully!",
+                data: newDeco
+            });
+        } catch (err) {
             return res.status(400).json({
                 success: false,
-                message: "No decos found!"
+                error: err.message
+            });
+        }    
+    }*/
+
+    //get decorations
+    export async function getDeco (req,res){
+
+        try{
+            const deco = await Decoration.find();
+
+            if(deco.length===0){
+                return res.status(400).json({
+                    success: false,
+                    message: "No decos found!"
+                });
+            }
+            return res.status(200).json({ success: true, deco });
+
+        }catch(err){
+            return res.status(404).json({ 
+                error: err.message 
             });
         }
-        return res.status(200).json({ success: true, deco });
-
-    }catch(err){
-        return res.status(404).json({ 
-            error: err.message 
-        });
     }
-}
 
-//update decorations
-export async function updateDeco(req,res){
-    try {
-        const { id } = req.params;
-        const updatedData = req.body;
+    //update decorations
+    export async function updateDeco(req,res){
+        try {
+            const { id } = req.params;
+            const updatedData = req.body;
 
-        const updatedDeco = await Decoration.findByIdAndUpdate(id, updatedData, { new: true });
+            const updatedDeco = await Decoration.findByIdAndUpdate(id, updatedData, { new: true });
 
-        if (!updateDeco){
-            return res.status(404).json({
+            if (!updatedDeco){
+                return res.status(404).json({
+                    success: false,
+                    message: "Decoration not found!"
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                updateDeco
+            });
+        } catch(err){
+            return res.status(400).json({
                 success: false,
-                message: "Decoration not found!"
+                error: err.message
             });
         }
-        return res.status(200).json({
-            success: true,
-            updateDeco
-        });
-    } catch(err){
-        return res.status(400).json({
-            success: false,
-            error: err.message
-        });
     }
-}
 
-//delete decorations
-export async function deleteDeco (req,res){
-    try{
-        const deletedDeco = await Decoration.findByIdAndDelete(req.params.id);
+    //delete decorations
+    export async function deleteDeco (req,res){
+        try{
+            const deletedDeco = await Decoration.findByIdAndDelete(req.params.id);
 
-        if (!deleteDeco){
-            return res.status(404).json({
+            if (!deletedDeco){
+                return res.status(404).json({
+                    success: false,
+                    message: "Decoration not found!"
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                deleteDeco  
+            });
+        } catch(err){
+            return res.status(400).json({
                 success: false,
-                message: "Decoration not found!"
+                error: err.message
             });
         }
-        return res.status(200).json({
-            success: true,
-            deleteDeco
-        });
-    } catch(err){
-        return res.status(400).json({
-            success: false,
-            error: err.message
-        });
     }
-}
