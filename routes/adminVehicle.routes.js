fcab57276f0186736334e88d893d121d42bf44eb
Gunathilaka K K N M <nimgunathilaka021@gmail.com>
2025-03-21 13:41:46 +0530

    Add view and delete routes for vehicles in adminVehicle.routes.js 👍

--- routes/adminVehicle.routes.js
+++ routes/adminVehicle.routes.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import { addVehicle } from '../controllers/adminVehicle.controller.js';
+import { addVehicle, getVehicles, deleteVehicle, updateVehicle } from '../controllers/adminVehicle.controller.js';
 
 const adminVehicleRouter = express.Router();
 
 adminVehicleRouter.post("/", addVehicle);
+adminVehicleRouter.get("/", getVehicles);
+adminVehicleRouter.delete("/:id", deleteVehicle);
+adminVehicleRouter.put("/:id", updateVehicle);
 
 export default adminVehicleRouter;
