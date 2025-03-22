c9bb559fef81b3c8e5c506a1483620cf26ed6d58
christine-samandi <christinefernando816@gmail.com>
2025-03-22 14:45:01 +0530

    refactor decorations routes to use shorter URLs

--- Backend/routes/decorationsRoute.js
+++ Backend/routes/decorationsRoute.js
@@ -5,19 +5,19 @@ const decoRouter = express.Router();
 
 //add decorations
 import { addDeco } from "../controllers/decorationsController.js";
-decoRouter.post("/deco/save", addDeco);
+decoRouter.post("/save", addDeco);
 
 //get decorations
 import { getDeco } from "../controllers/decorationsController.js";
-decoRouter.get("/deco/get", getDeco);
+decoRouter.get("/get", getDeco);
 
 //update decorations
 import { updateDeco } from "../controllers/decorationsController.js";
-decoRouter.put("/deco/update/:id", updateDeco);
+decoRouter.put("/update/:id", updateDeco);
 
 //delete decorations
 import { deleteDeco } from "../controllers/decorationsController.js";
-decoRouter.delete("/deco/delete/:id", deleteDeco);
+decoRouter.delete("/delete/:id", deleteDeco);
 
 export default decoRouter;
 
