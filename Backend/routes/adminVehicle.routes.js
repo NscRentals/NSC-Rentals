cd31c9fc19ed44fb1b9731d9a8de70080b669fd8
Gunathilaka K K N M <nimgunathilaka021@gmail.com>
2025-03-22 11:35:01 +0530

    Add admin vehicle routes for CRUD operations 👌

---
+++ Backend/routes/adminVehicle.routes.js
@@ -0,0 +1,13 @@
+
+import express from 'express';
+import { addVehicle, getVehicles, deleteVehicle, updateVehicle, getVehicle } from '../controllers/adminVehicle.controller.js';
+
+const adminVehicleRouter = express.Router();
+
+adminVehicleRouter.post("/", addVehicle);
+adminVehicleRouter.get("/", getVehicles);
+adminVehicleRouter.get("/:id", getVehicle);
+adminVehicleRouter.delete("/:id", deleteVehicle);
+adminVehicleRouter.put("/:id", updateVehicle);
+
+export default adminVehicleRouter;
