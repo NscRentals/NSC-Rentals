daef191c2ec254c395ee4bc9d217f62c1b0d7375
christine-samandi <christinefernando816@gmail.com>
2025-03-22 17:21:04 +0530

    refactor: Add specific decoration endpoint and update decorations routes

--- Backend/routes/decorationsRoute.js
+++ Backend/routes/decorationsRoute.js
@@ -1,15 +1,16 @@
 import express from "express";
-import Decoration from "../models/decorations.js";
+/*import Decoration from "../models/decorations.js";*/
 
 const decoRouter = express.Router();
 
 //add decorations
-import { addDeco } from "../controllers/decorationsController.js";
+import { addDeco, getDecoById } from "../controllers/decorationsController.js";
 decoRouter.post("/save", addDeco);
 
 //get decorations
 import { getDeco } from "../controllers/decorationsController.js";
 decoRouter.get("/get", getDeco);
+decoRouter.get("/get/:id", getDecoById); //specific decoration
 
 //update decorations
 import { updateDeco } from "../controllers/decorationsController.js";
