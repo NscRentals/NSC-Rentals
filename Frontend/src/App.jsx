9976e194c2ff481d9a2169e7d821d2e73bce4a54
christine-samandi <christinefernando816@gmail.com>
2025-03-23 11:26:25 +0530

    refactor: Update App component to use react-router-dom for routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,72 +1,110 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";  // Correct import
+import HomeD from "./components/HomeD";
+import CreateDeco from "./components/CreateDeco";
+import EditDeco from "./components/EditDeco";
+import DecoDetails from "./components/DecoDetails";
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      decorations: []
-    };
-
-  }
-
-  componentDidMount() {
-    this.retrieveDecorations();
-  }
-
-
-  retrieveDecorations() {
-    axios.get("http://localhost:4000/api/deco/get")
-      .then(res => {
-        if (res.data.success) {
-          this.setState({ 
-            decorations: res.data.deco 
-          });
-
-          console.log(this.state.decorations);
-
-        }
-      })
-      .catch(error => {
-        console.error("Error fetching decorations:", error);
-      });
-  }
-
   render() {
     return (
-      <div className = "container">
-        <p>All Decorations</p>
-        <table class = "table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Decoration ID</th>
-              <th scope="col">Type</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
+      <BrowserRouter>
+        <div className="container">
 
-          <tbody>
-            {this.state.decorations.map((decorations, index) => (
-              <tr>
-                <th scope="row">{index+1}</th>
-                <td>{decorations.dId}</td>
-                <td>{decorations.type}</td>
-                <td>
-                  <a className = "btn btn-warning" href = "#">
-                    <i className = "fas fa-edit"></i>&nbsp;Edit
-                  </a>
-                  &nbsp;
-                  <a className = "btn btn-danger" href = "#">
-                    <i className = "far fa-trash-alt"></i>&nbsp;Delete
-                  </a>  
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
+          {/* Corrected route setup */}
+          <Routes>
+            <Route path="/" element={<HomeD />} />  {/* Use element prop */}
+            <Route path="/add" element={<CreateDeco />} />
+            <Route path="/edit/:id" element={<EditDeco />} />
+            <Route path="/deco/:id" element={<DecoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  import React, { Component } from "react";
+//  import axios from "axios";
+
+//  export default class App extends Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+//       decorations: []
+//     };
+
+//   }
+
+//   componentDidMount() {
+//     this.retrieveDecorations();
+//   }
+
+
+//   retrieveDecorations() {
+//     axios.get("http://localhost:4000/api/deco/get")
+//       .then(res => {
+//         if (res.data.success) {
+//           this.setState({ 
+//             decorations: res.data.deco 
+//           });
+
+//           console.log(this.state.decorations);
+
+//         }
+//       })
+//       .catch(error => {
+//         console.error("Error fetching decorations:", error);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <div className = "container">
+//         <p>All Decorations</p>
+//         <table class = "table">
+//           <thead>
+//             <tr>
+//               <th scope="col">#</th>
+//               <th scope="col">Decoration ID</th>
+//               <th scope="col">Type</th>
+//               <th scope="col">Action</th>
+//             </tr>
+//           </thead>
+
+//           <tbody>
+//             {this.state.decorations.map((decorations, index) => (
+//               <tr>
+//                 <th scope="row">{index+1}</th>
+//                 <td>{decorations.dId}</td>
+//                 <td>{decorations.type}</td>
+//                 <td>
+//                   <a className = "btn btn-warning" href = "#">
+//                     <i className = "fas fa-edit"></i>&nbsp;Edit
+//                   </a>
+//                   &nbsp;
+//                   <a className = "btn btn-danger" href = "#">
+//                     <i className = "far fa-trash-alt"></i>&nbsp;Delete
+//                   </a>  
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     )
+//   }
+// }
