e806e1213f1a7f62ab3b4062a5d65dc27e14d399
christine-samandi <christinefernando816@gmail.com>
2025-03-23 11:26:35 +0530

    refactor: Create CreateDeco component for adding new decorations

---
+++ Frontend/src/components/CreateDeco.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class CreateDeco extends Component {
+    render(){
+        return (
+            <div>
+                <p>Create Decoration</p>
+            </div>
+        );
+    }
+}
+
