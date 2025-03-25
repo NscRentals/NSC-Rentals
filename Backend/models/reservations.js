9517745efea5546da5fdc9ff2d18724a69766e8c
christine-samandi <christinefernando816@gmail.com>
2025-03-25 19:45:28 +0530

    refactor: Add required fields to reservation model for reservation creation

--- Backend/models/reservations.js
+++ Backend/models/reservations.js
@@ -1,56 +1,70 @@
 import mongoose from "mongoose";
 
+
 const reservationSchema = new mongoose.Schema({
     rId: {
         type: String,
         required: true
     },
-    rType: {
-        type: Boolean, // True for WeddingCars, false for NormalCars
+    name: {
+        type: String,
         required: true
     },
-    time: {
+    email: {
         type: String,
-        required: true}
-    // },
-    // startDate: {
-    //     type: Date,
-    //     required: true
-    // },
-    // endDate: {
-    //     type: Date,
-    // },
-    // vehicleId: {
-    //     type: String,
-    //     required: true
-    // },
-    // decorations: {
-    //     type: Boolean,
-    //     required: true
-    // },
-    // decoType: {
-    //     type: String,
-    // },
-    // isVerified: {
-    //     type: Boolean, // True for Verified, False for Not Verified
-    //     required: true
-    // },
-    // price: {
-    //     type: Number,
-    //     required: true
-    // },
-    // driverReq: {
-    //     type: Boolean, // True for Driver Required, False for Not Required
-    //     required: true
-    // },
-    // driverId: {
-    //     type: String,
-    //     required: true
-    // },
-    // pickupLocation: {
-    //     type: String,
-    //     required: true
-    // },
+        required: true
+    },
+    rType: {
+        type: String,
+        enum : ["Wedding", "normal"],
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+    },
+    vehicleId: {
+        type: String,
+        required: true
+    },
+    decorations: {
+        type: Boolean,
+        required: true
+    },
+    decoType: {
+        type: String,
+    },
+    isVerified: {
+        type: Boolean, // True for Verified, False for Not Verified
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    driverReq: {
+        type: Boolean, // True for Driver Required, False for Not Required
+        required: true
+    },
+    driverId: {
+        type: String,
+        required: true
+    },
+    pickupLocation: {
+        type: String,
+        required: true
+    },
     
 });
 
